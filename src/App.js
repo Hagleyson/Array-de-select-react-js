3d32b65db968ee83b5f453fbf980c7a984a7c52d
hagleyson <hagleyson@gmail.com>
2021-12-01 23:43:47 -0300

    finalizado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [selects, setSelects] = useState(["teste"]);
+
+  const update = (i, e) => {
+    const newSelects = [...selects];
+    newSelects[i] = e.target.value;
+    setSelects(newSelects);
+  };
+  const remove = (i) => {
+    const newSelects = [...selects];
+    newSelects.splice(i, 1);
+    setSelects(newSelects);
+  };
+  const add = () => {
+    const newSelects = [...selects];
+    newSelects.push("teste");
+    setSelects(newSelects);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {selects && selects.length > 0 ? (
+        selects.map((s, i) => (
+          <div key={i}>
+            <select value={selects[i]} onChange={(e) => update(i, e)}>
+              <option value="teste">teste</option>
+              <option value="teste1">teste1</option>
+              <option value="teste2">teste2</option>
+            </select>
+            <button onClick={() => add()}>Adicionar</button>
+            <button onClick={() => remove(i)}>Remover</button>
+          </div>
+        ))
+      ) : (
+        <button onClick={() => add()}>teste</button>
+      )}
+      <button onClick={() => alert(selects)}>Enviar</button>
+    </>
   );
 }
 
